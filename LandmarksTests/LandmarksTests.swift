f69c3cf57c70e484f382c3e9e0c3b71fb2c0ff7c
hyeeejin2 <gpwlwlgml2@gmail.com>
2024-11-01 15:52:29 +0900

    Initial Commit

---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by 김혜진 on 11/1/24.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
